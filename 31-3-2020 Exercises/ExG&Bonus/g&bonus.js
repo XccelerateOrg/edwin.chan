87e0da6158e7c3b1ef454d9152c520175cb99779
edwin9876 <edwin9876@ymail.com>
2020-03-31 15:41:49 +0800

    submit31-3-2020exercise

---
+++ 31-3-2020 Exercises/ExG&Bonus/g&bonus.js
@@ -0,0 +1,16 @@
+//Given that x = 8 and y = 3. 
+// x == y       =>false
+// x && y < 10  =>true
+// x || y <= 8  =>true
+// y === '3'    =>false
+// !(x == 5)    =>true
+
+// Bonus Exercise
+
+// The function below is not working properly, can you fix it?
+
+function area(height,width) {
+   return height * width;
+}
+
+console.log(area(2,3));
