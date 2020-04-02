633a0a15bc8fa89851326e05acd19ad64d7b4acf
edwin9876 <edwin9876@ymail.com>
2020-04-02 21:25:35 +0800

    2-4-2020

--- 1-4-2020 Exercises/ExD/ExD.js
+++ 1-4-2020 Exercises/ExD/ExD.js
@@ -1,5 +1,5 @@
 function filter(num){
-    (num<0)?num=0:(typeof num!=="number")?num="ERROR":(num>=1000000)?num:num;
+    (num<=0 || typeof num!=="number")?num="ERROR":(num>=1000000)?num:num;
     while (num<1000000&&num>0){
         num=num*10;
     }
@@ -9,3 +9,4 @@ function filter(num){
 console.log(filter("apple"));
 console.log(filter(1200));
 console.log(filter(-200));
+console.log(filter(2833200));
