2e43304d416abb635927038843f4c76da4a38e24
edwin9876 <edwin9876@ymail.com>
2020-04-06 17:18:56 +0800

    updated to day4 exH

--- Day4/ExH.js
+++ Day4/ExH.js
@@ -1,16 +1,15 @@
-const letterArray = [...('abcdefghijklmnopqrstuvwxyz')];
-
+ 
 function move(str){
     let arr=[];
     for(i=0;i<str.length;i++){
         let indexNum = str.charCodeAt(i)+10;
         if(indexNum>122){
-        
+            indexNum2=indexNum-122+96;
+            arr.push(indexNum2);
         } else {
-            arr.push(str.charCodeAt(i)+10);
+            arr.push(indexNum);
         }
     }
-    
-    return arr;
+    return String.fromCharCode(...arr).split("");
 }
-console.log(move('abd'));
+console.log(move('zbd'));
